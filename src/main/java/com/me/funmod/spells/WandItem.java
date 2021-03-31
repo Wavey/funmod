f87b3d632ce137a055d4611d04d6179932bcfdd5
Dave Dixon <dave.dixon@gmail.com>
2021-03-30 21:57:04 -0700

    Add tooltip and refactor GetOrCreateSpells

--- src/main/java/com/me/funmod/spells/WandItem.java
+++ src/main/java/com/me/funmod/spells/WandItem.java
@@ -1,5 +1,8 @@
 package com.me.funmod.spells;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.inventory.Inventories;
@@ -7,12 +10,15 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,8 +36,21 @@ public class WandItem extends Item {
 
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
+
+        List<Spell> spells = WandItem.GetOrCreateSpells(itemStack, true);
+        System.out.println("Casting: " + spells.get(0).getName() + " - " + spells.get(1).getName() + " - " + spells.get(2).getName());
+
+        return TypedActionResult.pass(itemStack);
+
+    }
+    protected static List<Spell>GetOrCreateSpells(ItemStack itemStack, boolean createIfNeeded) {
+        ArrayList<Spell> spells = new ArrayList<Spell>();
         CompoundTag tag = itemStack.getOrCreateSubTag("Spells");
         if(tag.isEmpty()) {
+            if(! createIfNeeded) {
+                // don't create, just return
+                return spells;
+            }
             // we need to put in some spells, since we have not already
             // initialized this yet
             tag.put("spell1", new Spell(WandItem.names.get(random.nextInt(names.size() - 1))).toTag());
@@ -40,13 +59,19 @@ public class WandItem extends Item {
             System.out.println("Empty wand.. adding spells");
         }
 
-        Spell spell1 = Spell.fromTag(tag.getCompound("spell1"));
-        Spell spell2 = Spell.fromTag(tag.getCompound("spell2"));
-        Spell spell3 = Spell.fromTag(tag.getCompound("spell3"));
+        spells.add(Spell.fromTag(tag.getCompound("spell1")));
+        spells.add(Spell.fromTag(tag.getCompound("spell2")));
+        spells.add(Spell.fromTag(tag.getCompound("spell3")));
 
-        System.out.println("Casting: " + spell1.getName() + " - " + spell2.getName() + " - " + spell3.getName());
+        return spells;
 
-        return TypedActionResult.pass(itemStack);
 
+    }
+    @Environment(EnvType.CLIENT)
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        List<Spell> spells = WandItem.GetOrCreateSpells(stack, false);
+        if(!spells.isEmpty()) {
+            tooltip.add(new LiteralText(spells.get(0).getName() + " - " + spells.get(1).getName() + " - " + spells.get(2).getName()));
+        }
     }
 }
