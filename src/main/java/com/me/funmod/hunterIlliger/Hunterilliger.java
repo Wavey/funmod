e15053436b8796b7ff2f490245f8fe181b20eae8
Finn Murphy <finnomurphy@gmail.com>
2021-06-14 17:59:22 -0700

    updated hunter

--- src/main/java/com/me/funmod/hunterIlliger/Hunterilliger.java
+++ src/main/java/com/me/funmod/hunterIlliger/Hunterilliger.java
@@ -3,9 +3,12 @@ package com.me.funmod.hunterIlliger;
 import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.FollowTargetGoal;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.IllagerEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PillagerEntity;
 import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.PlayerEntity;
@@ -38,6 +41,15 @@ public class Hunterilliger extends PillagerEntity {
         projectile.setVelocity((double)vector3f.getX(), (double)vector3f.getY(), (double)vector3f.getZ(), speed, (float)(14 - entity.world.getDifficulty().getId() * 4));
         entity.playSound(SoundEvents.ITEM_CROSSBOW_SHOOT, 1.0F, 1.0F / (entity.getRandom().nextFloat() * 0.4F + 0.8F));
     }
+    public static DefaultAttributeContainer.Builder createHostileAttributes() {
+        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_FOLLOW_RANGE,75);
+    }
+
+    @Override
+    public boolean canLead() {
+        return false;
+    }
+
     public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable CompoundTag entityTag) {
 
 
