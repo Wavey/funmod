4bd2385742cdcb1a6e89dac38224779f0273d6f9
Finn Murphy <finnomurphy@gmail.com>
2021-11-12 15:35:06 -0800

    mimic ai improvements

--- src/main/java/com/me/funmod/mixins/AnimalEntityMixin.java
+++ src/main/java/com/me/funmod/mixins/AnimalEntityMixin.java
@@ -1,24 +1,91 @@
 package com.me.funmod.mixins;
 
+import com.me.funmod.FunMod;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.ai.pathing.PathNodeType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
 
 @Mixin(AnimalEntity.class)
 public abstract class AnimalEntityMixin extends PassiveEntity {
+    //private static Random rand = new Random();
+
+    private static final TrackedData<Boolean> MIMIC;
+
     protected AnimalEntityMixin(EntityType<? extends PassiveEntity> entityType, World world) {
         super(entityType, world);
         this.setPathfindingPenalty(PathNodeType.DANGER_FIRE, 16.0F);
         this.setPathfindingPenalty(PathNodeType.DAMAGE_FIRE, -1.0F);
     }
+
+    static {
+        MIMIC = DataTracker.registerData(AnimalEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    }
+
+    protected void initDataTracker() {
+        super.initDataTracker();
+
+        AnimalEntity This = (AnimalEntity)(Object) this;
+        boolean canbeMimic = This instanceof CowEntity || This instanceof SheepEntity || This instanceof PigEntity;
+        boolean m = random.nextInt(5) == 1 && !this.isBaby() && canbeMimic;
+
+        this.dataTracker.startTracking(MIMIC,m );
+    }
+
+    @Inject(at = @At("HEAD"), method = "mobTick()V", cancellable = true)
+    private void onMobTick( CallbackInfo info) {
+        AnimalEntity This = (AnimalEntity)(Object) this;
+        if (this.dataTracker.get(this.MIMIC) == true && (this.getAttacker() instanceof PlayerEntity)){
+            this.convertTo(FunMod.MIMIC,false);
+        }
+        if (This instanceof CowEntity && this.dataTracker.get(this.MIMIC) == true){
+            this.goalSelector.clear();
+            this.goalSelector.add(0, new SwimGoal(this));
+            this.goalSelector.add(1, new EscapeDangerGoal(this, 2.0D));
+            this.goalSelector.add(3, new TemptGoal(this, 1.25D, Ingredient.ofItems(new ItemConvertible[]{Items.WHEAT}), false));
+            this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
+            this.goalSelector.add(7, new LookAroundGoal(this));
+            this.ambientSoundChance = 0;
+        }
+        if (This instanceof SheepEntity && this.dataTracker.get(this.MIMIC) == true){
+            this.goalSelector.clear();
+            this.goalSelector.add(0, new SwimGoal(this));
+            this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25D));
+            this.goalSelector.add(3, new TemptGoal(this, 1.1D, Ingredient.ofItems(new ItemConvertible[]{Items.WHEAT}), false));
+            this.goalSelector.add(6, new WanderAroundFarGoal(this, 1.0D));
+            this.goalSelector.add(8, new LookAroundGoal(this));
+            this.ambientSoundChance = 0;
+        }
+        if (This instanceof PigEntity && this.dataTracker.get(this.MIMIC) == true){
+            this.goalSelector.clear();
+            this.goalSelector.add(0, new SwimGoal(this));
+            this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25D));
+            this.goalSelector.add(4, new TemptGoal(this, 1.2D, Ingredient.ofItems(new ItemConvertible[]{Items.CARROT_ON_A_STICK}), false));
+            this.goalSelector.add(6, new WanderAroundFarGoal(this, 1.0D));
+            this.goalSelector.add(8, new LookAroundGoal(this));
+            this.ambientSoundChance = 0;
+        }
+    }
+
     @Nullable
     public <T extends AnimalEntity> T convertAnimalTo(EntityType<T> entityType, boolean keepEquipment) {
         if (this.isRemoved()) {
--- src/main/java/com/me/funmod/mixins/CowMixin.java
+++ src/main/java/com/me/funmod/mixins/CowMixin.java
@@ -1,12 +1,14 @@
 package com.me.funmod.mixins;
 
 
+import com.me.funmod.FunMod;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -23,25 +25,33 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Random;
+import com.me.funmod.mimic.Mimic;
 
 @Mixin(CowEntity.class)
 public abstract class CowMixin extends AnimalEntity {
 
-    private static Random random = new Random();
+  //  private static Random random = new Random();
 
-    private static final TrackedData<Boolean> MIMIC;
+  // private static final TrackedData<Boolean> MIMIC;
 
     protected CowMixin(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
-    static {
-        MIMIC = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
-    }
-    protected void initDataTracker() {
-        super.initDataTracker();
+    //static {
+       // MIMIC = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    //}
+  //  protected void initDataTracker() {
+     //   super.initDataTracker();
 
-        this.dataTracker.startTracking(MIMIC, random.nextInt(5) == 1 && !this.isBaby());
-    }
+     //   this.dataTracker.startTracking(MIMIC, random.nextInt(5) == 1 && !this.isBaby());
+   // }
+    //@Inject(at = @At("HEAD"), method = "tick()V", cancellable = true)
+   // private void onTick( CallbackInfo info) {
+    //    if (this.dataTracker.get(this.MIMIC) == true && (this.getAttacker() instanceof PlayerEntity)){
+     //       this.convertTo(FunMod.MIMIC,false);
+     //   }
+   // }
+
    // @Inject(at = @At("HEAD"), method = "initGoals()V", cancellable = true)
    // protected void initGoals(CallbackInfo  info) {
        // this.goalSelector.add(0, new SwimGoal(this));
@@ -54,21 +64,21 @@ public abstract class CowMixin extends AnimalEntity {
     //}
 
 
-    @Inject(at = @At("HEAD"), method = "getAmbientSound()Lnet/minecraft/sound/SoundEvent;", cancellable = true)
-    protected void getAmbientSound(CallbackInfoReturnable<SoundEvent> info) {
-        if (this.dataTracker.get(this.MIMIC) == true){
-            this.goalSelector.clear();
-            this.goalSelector.add(0, new SwimGoal(this));
-            this.goalSelector.add(1, new EscapeDangerGoal(this, 1.4D));
-            this.goalSelector.add(3, new TemptGoal(this, 1.25D, Ingredient.ofItems(new ItemConvertible[]{Items.WHEAT}), false));
-            this.goalSelector.add(4, new FollowParentGoal(this, 1.25D));
-            this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
-            this.goalSelector.add(7, new LookAroundGoal(this));
-            info.setReturnValue(SoundEvents.ENTITY_BEE_DEATH);
-        }else{
-            info.setReturnValue((SoundEvents.ENTITY_COW_AMBIENT));
-        }
+    //@Inject(at = @At("HEAD"), method = "getAmbientSound()Lnet/minecraft/sound/SoundEvent;", cancellable = true)
+    //protected void getAmbientSound(CallbackInfoReturnable<SoundEvent> info) {
+      //  if (this.dataTracker.get(this.MIMIC) == true){
+       //     this.goalSelector.clear();
+        //    this.goalSelector.add(0, new SwimGoal(this));
+        //    this.goalSelector.add(1, new EscapeDangerGoal(this, 1.4D));
+        //    this.goalSelector.add(3, new TemptGoal(this, 1.25D, Ingredient.ofItems(new ItemConvertible[]{Items.WHEAT}), false));
+         //   this.goalSelector.add(4, new FollowParentGoal(this, 1.25D));
+        //    this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
+         //   this.goalSelector.add(7, new LookAroundGoal(this));
+       //     info.setReturnValue(SoundEvents.ENTITY_BEE_DEATH);
+      //  }else{
+       //     info.setReturnValue((SoundEvents.ENTITY_COW_AMBIENT));
+      //  }
 
     }
 
-}
+//}
